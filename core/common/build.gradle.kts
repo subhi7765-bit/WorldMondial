667064a974fd744a54dec7c9cf3b16f3c8ea8ca9
subhi7765-bit <subhi7765@gmail.com>
2026-06-04 09:41:06 +0300

    Update build.gradle.kts

--- core/common/build.gradle.kts
+++ core/common/build.gradle.kts
@@ -1,14 +1,20 @@
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt)
 }
 
 android {
-    namespace = "sa.mondial.world.core.domain"
+    namespace = "sa.mondial.world.core.common"
     compileSdk = 35
     defaultConfig { minSdk = 26 }
 }
 
 dependencies {
     implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.compiler)
+    implementation(libs.retrofit.core)
 }
