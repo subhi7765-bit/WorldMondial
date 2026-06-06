1717566fd3867c28a406fe46f20e4e4515f63ea5
subhi7765-bit <subhi7765@gmail.com>
2026-06-06 21:26:03 +0300

    Update DispatchersModule.kt

--- core/di/src/main/java/sa/mondial/world/core/di/DispatchersModule.kt
+++ core/di/src/main/java/sa/mondial/world/core/di/DispatchersModule.kt
@@ -9,14 +9,23 @@ import kotlinx.coroutines.Dispatchers
 import javax.inject.Qualifier
 import javax.inject.Singleton
 
+/**
+ * Qualifier for background thread execution context optimized for Disk and Network I/O operations.
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class IoDispatcher
 
+/**
+ * Qualifier for UI thread execution context primarily managing Compose layouts and view interactions.
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class MainDispatcher
 
+/**
+ * Qualifier for CPU-intensive background tasks such as list sorting, cryptographic operations, or JSON parsing.
+ */
 @Qualifier
 @Retention(AnnotationRetention.BINARY)
 annotation class DefaultDispatcher
