5ce0804a029cdad0b51fb74792be20a83c13f725
subhi7765-bit <subhi7765@gmail.com>
2026-06-10 18:14:05 +0300

    Update GetMatchesUseCase.kt

--- feature/matches/src/main/java/sa/mondial/world/feature/matches/domain/GetMatchesUseCase.kt
+++ feature/matches/src/main/java/sa/mondial/world/feature/matches/domain/GetMatchesUseCase.kt
@@ -1,7 +1,6 @@
 package sa.mondial.world.feature.matches.domain
 
 import kotlinx.coroutines.flow.Flow
-import androidx.paging.PagingData
 import sa.mondial.world.core.common.Result
 import sa.mondial.world.core.domain.Match
 import sa.mondial.world.feature.matches.data.MatchesRepository
@@ -12,10 +11,5 @@ class GetMatchesUseCase @Inject constructor(
 ) {
     operator fun invoke(forceRefresh: Boolean = false): Flow<Result<List<Match>>> {
         return matchesRepository.getStreamedMatches(forceRefresh)
-    }
-
-    // THE FIX: Added datePrefix parameter
-    fun getPaged(forceRefresh: Boolean = false, datePrefix: String): Flow<PagingData<Match>> {
-        return matchesRepository.getPagedMatches(forceRefresh, datePrefix)
     }
 }
